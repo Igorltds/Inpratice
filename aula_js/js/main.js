ad970d73a94f81eeaa3f16578e02df10a0f764ba
Igorltdz <igorltdz@gmail.com>
2021-05-07 19:55:26 -0300

    js aula01

--- aula_js/js/main.js
+++ aula_js/js/main.js
@@ -1,5 +1,4 @@
-let name = "Igor Luan";
-let age = 29;
-let age2 = age - 10; 
-alert("ele tem " + age2 + ", o " + name)
-console.log(age2-age)
+let lista = ["banana", "pera"]
+lista.push ("uva")
+
+console.log(lista)
