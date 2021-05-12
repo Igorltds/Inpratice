ea0373db96e5dbe367b04fee262b229a3c8a7392
Igorltdz <igorltdz@gmail.com>
2021-05-12 19:22:33 -0300

    aula_js

--- aula_js/js/main.js
+++ aula_js/js/main.js
@@ -1,4 +1,19 @@
-let lista = ["banana", "pera"]
+/*
+let name = "Igor Luan";
+let age = 29;
+let age2 = age - 10; 
+alert("ele tem " + age2 + ", o " + name)
+console.log(age2-age)
+*/
+/*
+let lista = ["banana", "pera"]  
 lista.push ("uva")
 
 console.log(lista)
+*/
+/*
+let var1 =  'ma'
+let var2 = 'oe \n'
+console.log(var1 + var2 + 1425+'-'+ 1519);
+*/
+
